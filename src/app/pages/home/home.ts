29f085289b5c4304218a8dbeeaa92500262bef9b
RogerioNeto <joserogeriofonsecauzl@gmail.com>
2026-06-17 20:23:56 -0300

    feat: add SVG animation on home screen

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, afterNextRender } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LucideArrowBigRight } from '@lucide/angular';
 
+declare var anime: any;
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,34 @@ import { LucideArrowBigRight } from '@lucide/angular';
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class Home {}
+export class Home {
+
+  constructor() {
+    afterNextRender(() => {
+      
+      // O setTimeout garante que o SVG já foi renderizado na tela antes do cálculo
+      setTimeout(() => {
+        // 1. Força o Anime.js a calcular e animar o traçado do zero
+        anime({
+          targets: '.svg-demonstracao path',
+          strokeDashoffset: [anime.setDashoffset, 0],
+          easing: 'easeInOutSine',
+          duration: 1200,
+          direction: 'alternate',
+          loop: true
+        });
+
+        // 2. Animação dos textos e botões logo em seguida
+        anime({
+          targets: '.home-container h1, .home-container p, .home-container .card-botao',
+          translateY: [40, 0],
+          opacity: [0, 1],
+          duration: 800,
+          delay: anime.stagger(100, { start: 200 }),
+          easing: 'easeOutQuad'
+        });
+      }, 50);
+
+    });
+  }
+}
